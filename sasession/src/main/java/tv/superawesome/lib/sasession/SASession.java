897ad32d24f2f885028aa6caf9d451da9228ae83
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-07-15 14:33:43 +0100

    0.1.0

---
+++ sasession/src/main/java/tv/superawesome/lib/sasession/SASession.java
@@ -0,0 +1,73 @@
+package tv.superawesome.lib.sasession;
+
+import java.util.HashMap;
+
+/**
+ * Created by gabriel.coman on 15/07/16.
+ */
+public class SASession {
+
+    // constants
+    private final static String PRODUCTION_URL = "https://ads.superawesome.tv/v2";
+    private final static String STAGING_URL = "https://ads.staging.superawesome.tv/v2";
+
+    // singleton instance
+    private static SASession instance = new SASession();
+
+    // state variables
+    private String baseUrl;
+    private boolean testEnabled;
+    private int dauId;
+
+    // constructor
+    private SASession() {
+        setConfigurationProduction();
+        setTestDisabled();
+        setDauId(0);
+    }
+
+    // public singleton functions
+    public static SASession getInstance(){
+        return instance;
+    }
+
+    // setters
+
+    public void setConfigurationProduction () {
+        baseUrl = PRODUCTION_URL;
+    }
+
+    public void setConfigurationStaging () {
+        baseUrl = STAGING_URL;
+    }
+
+    public void setTestEnabled () {
+        testEnabled = true;
+    }
+
+    public void setTestDisabled () {
+        testEnabled = false;
+    }
+
+    public void setTest(boolean testEnabled) {
+        this.testEnabled = testEnabled;
+    }
+
+    public void setDauId(int dauId) {
+        this.dauId = dauId;
+    }
+
+    // getters
+
+    public String getBaseUrl () {
+        return baseUrl;
+    }
+
+    public boolean isTestEnabled () {
+        return testEnabled;
+    }
+
+    public int getDauId () {
+        return dauId;
+    }
+}
