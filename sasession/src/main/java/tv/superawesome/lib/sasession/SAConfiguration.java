8231da709fa9debf4f62127ad7a9d0bf3a3ccf4a
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-01-03 13:41:50 +0000

    0.2.3 - Updated the SAConfiguration enum to contain a static init from value method (with value being an integer)

--- sasession/src/main/java/tv/superawesome/lib/sasession/SAConfiguration.java
+++ sasession/src/main/java/tv/superawesome/lib/sasession/SAConfiguration.java
@@ -4,6 +4,20 @@ package tv.superawesome.lib.sasession;
  * Created by gabriel.coman on 15/09/16.
  */
 public enum SAConfiguration {
-    PRODUCTION,
-    STAGING
+    PRODUCTION(0),
+    STAGING(1);
+
+    private final int value;
+    SAConfiguration(int value) {
+        this.value = value;
+    }
+
+    public static SAConfiguration fromValue (int value) {
+        if (value == 0) return PRODUCTION;
+        return STAGING;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
